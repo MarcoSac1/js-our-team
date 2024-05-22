fc84b085374811cec478d61123f4ae37ba2033d7
MarcoSac1 <marcosaccone8@gmail.com>
2024-05-22 15:39:27 +0200

    created object Array

--- README.md
+++ README.md
@@ -12,3 +12,19 @@ BONUS 1:
 Trasformare la stringa foto in una immagine effettiva
 BONUS 2:
 Organizzare i singoli membri in card/schede
+
+Wayne Barnett	Founder & CEO	wayne-barnett-founder-ceo.jpg
+Angela Caroll	Chief Editor	angela-caroll-chief-editor.jpg
+Walter Gordon	Office Manager	walter-gordon-office-manager.jpg
+Angela Lopez	Social Media Manager	angela-lopez-social-media-manager.jpg
+Scott Estrada	Developer	scott-estrada-developer.jpg
+Barbara Ramos	Graphic Designer	barbara-ramos-graphic-designer.jpg
+<!-- workflow -->
+
+
+<!-- 
+- creo un array di oggetti con al interno ogni mebro del e le informazioni desiderate
+- faccio un console.log per verificare tutte le informazioni inserite
+- creo un elemento nel dom per ogni array 
+- lo aggiungo al dom
+ -->
---
+++ js/script.js
@@ -0,0 +1,44 @@
+// - creo un array di oggetti con al interno ogni mebro del team e le informazioni desiderate
+
+const team = [
+    {
+        nome :'Wayne Barnett',
+        ruolo :'Founder & CEO',
+        foto :'./img/wayne-barnett-founder-ceo.jpg',
+    },
+    {
+        nome :'Angela Caroll',
+        ruolo :'Chief Editor',
+        foto :'./img/angela-caroll-chief-editor.jpg',
+    },
+    {
+        nome :'Walter Gordon',
+        ruolo :'Office Manager',
+        foto :'./img/walter-gordon-office-manager.jpg',
+    },
+    {
+        nome :'Angela Lopez',
+        ruolo :'Social Media Manager',
+        foto :'./img/angela-lopez-social-media-manager.jpg',
+    },
+    {
+        nome :'Scott Estrada',
+        ruolo :'Developer',
+        foto :'./img/scott-estrada-developer.jpg',
+    },
+    {
+        nome :'Barbara Ramos',
+        ruolo :'Graphic Designer',
+        foto :'./img/barbara-ramos-graphic-designer.jpg',
+    },
+
+];
+
+console.log(team);
+// - faccio un console.log per verificare tutte le informazioni inserite
+
+
+// - creo un elemento nel dom per ogni array 
+
+
+// - lo aggiungo al dom
